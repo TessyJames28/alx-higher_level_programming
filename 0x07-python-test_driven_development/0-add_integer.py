11d98698a657b0eed078f327413752bc3a6c6a76
Tessyjames28 <tessyjames28@gmail.com>
2022-12-30 06:11:32 +0100

    function that add two integers based on TDD

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,33 @@
+"""this module adds two integers a and b"""
+
+
+def add_integer(a, b=98):
+    """
+    function that adds two integers a and b and returns result
+
+    Arguments:
+        a (int): `a`
+        b (optional, int): `b`
+
+    Raises:
+        TypeError: a must be an int
+        TypeError: b must be an int
+
+    Returns:
+        int: `a` + `b`
+    """
+
+    if isinstance(a, float):
+        a = int(a)
+    if isinstance(b, float):
+        b = int(b)
+    if not isinstance(a, int):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, int):
+        raise TypeError("b must be an integer")
+
+    return a + b
+
+
+if __name__ == "__main__":
+    doctest.testfile("./tests/0-add_integer.txt")
---
+++ 0x07-python-test_driven_development/README.md
@@ -0,0 +1,1 @@
+This directory contains tasks on Test Driven Development
