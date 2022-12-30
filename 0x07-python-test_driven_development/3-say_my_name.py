f3a91c5d5fba39f62b2883db714d91c792011ca9
Tessyjames28 <tessyjames28@gmail.com>
2022-12-30 14:32:46 +0100

    function test based on TDD

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,26 @@
+"""module that write the first and last name"""
+
+
+def say_my_name(first_name, last_name=""):
+    """
+    function that prints first and last name as argument
+
+    Arguments:
+        first_name (string): `first_name` mandatory
+        last_name (string): `last_name` optional
+
+    Raises:
+        TypeError: first_name must be a string
+        TypeError: last_name must be a string
+
+    Return:
+        Nothing
+    """
+
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+
+    print("My name is {} {}".format(first_name, last_name))
