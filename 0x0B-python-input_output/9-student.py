578712071072cd9f98d2d81ed80378a198f8157c
Tessyjames28 <tessyjames28@gmail.com>
2023-01-11 08:26:34 +0100

    a class student that translates to JSON

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""a class Student"""
+
+
+class Student:
+    """A class student with public instance fisrt_name, last_name, age"""
+    def __init__(self, first_name, last_name, age):
+        """
+        Arguments:
+            first_name - string
+            last_name - string
+            age - int
+        Returns:
+            retirves a dictionary representation of a Student instance
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        return self.__dict__
