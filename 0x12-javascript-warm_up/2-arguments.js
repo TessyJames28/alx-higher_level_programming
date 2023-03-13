3c07ace1dbdf051fb98bb7346785341f6394e1b5
Tessyjames28 <tessyjames28@gmail.com>
2023-03-14 00:33:16 +0100

    script that uses argument vector

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// Script that prints a message depending on the number of args passed
+
+if (process.argv.length === 2) {
+  console.log('No argument');
+} else if (process.argv.length === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
