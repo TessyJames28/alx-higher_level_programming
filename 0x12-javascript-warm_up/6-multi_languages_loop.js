45d81a56a2b1d1040c302ce52efd401761163e89
Tessyjames28 <tessyjames28@gmail.com>
2023-03-14 02:14:47 +0100

    array of string and loop

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// script that prints 3 lines using array of string and loop
+
+const arrString = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (let i = 0; i < arrString.length; i++) {
+  console.log(arrString[i]);
+}
