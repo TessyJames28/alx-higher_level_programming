8ce497bf7d60ea919613b9e6086a9fddc9af59e0
TessyJames28 <tessyjames28@gmail.com>
2023-03-18 00:19:17 +0100

    filtered from the commandline

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -12,7 +12,8 @@ if __name__ == "__main__":
                          passwd=sys.argv[2], db=sys.argv[3])
 
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name = '{}'".format(sys.argv[4]))
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'"
+                .format(sys.argv[4]))
 
     query_row = cur.fetchall()
     for row in query_row:
