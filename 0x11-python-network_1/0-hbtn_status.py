1d56229aeb6bc88af941f1f465113d413ec0189e
TessyJames28 <tessyjames28@gmail.com>
2023-04-01 09:59:38 +0100

    python script that fetches request

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# Python script that fetches an internet resources
+import urllib.request
+
+
+if __name__ == "__main__":
+
+    with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as res:
+        response = res.read()
+        print("Body respose:")
+        print("\t- type: {}".format(type(response)))
+        print("\t- content: {}".format(response))
+        print("\t- utf8 content: {}".format(response.decode("utf8")))
