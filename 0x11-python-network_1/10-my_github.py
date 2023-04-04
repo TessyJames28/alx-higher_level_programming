bf847e545531bccae4d0fd08b1b913ee1d21d0d4
TessyJames28 <tessyjames28@gmail.com>
2023-04-04 23:08:24 +0100

    Used GitHub API to retrieve a user id

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Python script that takes your GitHub credentials (username and password)
+and uses the GitHub API to display your id
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    user = sys.argv[1]
+    passwd = sys.argv[2]
+    r = requests.get("https://api.github.com/user", auth=(user, passwd))
+    print(r.json().get("id"))
