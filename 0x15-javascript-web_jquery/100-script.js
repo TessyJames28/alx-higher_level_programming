bde58e5b3a633f2ee3e75e84654af25c5f1564ef
TessyJames28 <tessyjames28@gmail.com>
2023-05-04 12:02:15 +0100

    Select change header element with js imported from the <head> tag

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,6 @@
+// JavaScript script that updates the text color of the
+// <header> element to red (#FF0000)
+document.addEventListener('DOMContentLoaded', function () {
+  const value = document.querySelector('header');
+  value.style.color = '#FF0000';
+});
